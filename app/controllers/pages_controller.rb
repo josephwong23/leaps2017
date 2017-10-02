05a616b4b6908ba15b62bc9801ba0cb047298924
Joseph Wong <josephwong@Joseph-Wongs-MacBook-Pro.local>
2017-10-02 18:00:58 +0800

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,44 @@
+class PagesController < ApplicationController
+  before_action :require_login, except: [:login, :authenticate]
+
+  def home
+  end
+
+  def workshops
+  end
+
+  def info
+  end
+
+  def registration
+  end
+
+  def speakers
+  end
+
+  def login
+    render 'login', layout: false
+  end
+
+  def logout
+    cookies[:ytl] = nil
+    redirect_to root_path
+  end
+
+  def authenticate
+    input = params[:passphrase]
+    if input == "SINCE1955"
+      cookies[:ytl] = {
+        value: 'cookie',
+        expires: 1.month.from_now
+      }
+    end
+    redirect_to root_path
+  end
+
+  def require_login
+    if cookies[:ytl] != "cookie"
+      redirect_to '/login'
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  root 'pages#home'
+  get '/home', to: 'pages#home'
+  get '/workshops', to: 'pages#workshops'
+  get '/speakers', to: 'pages#speakers'
+  get '/registration', to: 'pages#registration'
+  get '/info', to: 'pages#info'
+  get '/login', to: 'pages#login'
+  get '/logout', to: 'pages#logout'
+  post '/authenticate', to: 'pages#authenticate'
+end
