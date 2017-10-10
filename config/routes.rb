b5d1dbfad5bf70f5291b7e706f4a1d61ba79023c
Joseph Wong <josephwong@Joseph-Wongs-MacBook-Pro.local>
2017-10-10 17:12:41 +0800

    latest updates

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '/info', to: 'pages#info'
   get '/login', to: 'pages#login'
   get '/logout', to: 'pages#logout'
+  get '/afterparty', to: 'pages#afterparty'
   post '/authenticate', to: 'pages#authenticate'
 end
